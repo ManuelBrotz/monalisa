6649aa1a508b828f8ec1a250f2c76008c5ea9e6f
Manuel Brotz <manu.brotz@gmx.ch>
2013-10-03 16:24:19 +0200

    Introduced new class OldFormatConverter.

--- src/ch/brotzilla/monalisa/db/Database.java
+++ src/ch/brotzilla/monalisa/db/Database.java
@@ -1,10 +1,6 @@
 package ch.brotzilla.monalisa.db;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.tmatesoft.sqljet.core.SqlJetException;
 import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
@@ -16,7 +12,6 @@ import ch.brotzilla.monalisa.db.DatabaseSchema.TblGenomes;
 import ch.brotzilla.monalisa.db.schema.Index;
 import ch.brotzilla.monalisa.db.schema.Table;
 import ch.brotzilla.monalisa.evolution.genes.Genome;
-import ch.brotzilla.monalisa.io.TextReader;
 
 import com.google.common.base.Preconditions;
 
@@ -69,36 +64,9 @@ public class Database {
         }).getResult();
     }
     
-    public static void convertOldStorageFormat(File folder, File dbFile) throws SqlJetException, IOException {
-        Preconditions.checkNotNull(folder, "The parameter 'folder' must not be null");
-        Preconditions.checkArgument(folder.isDirectory(), "The parameter 'folder' has to be a directory");
-        
-        final LinkedList<File> files = new LinkedList<File>();
-        folder.listFiles(new FileLister(files));
-        
-        final SqlJetDb db = createDatabase(dbFile);
-        try {
-            final TextReader txt = new TextReader(1024 * 100);
-            new Transaction<Void>(db, SqlJetTransactionMode.WRITE) {
-                @Override
-                public Void run() throws Exception {
-                    final ISqlJetTable table = db.getTable(DatabaseSchema.tblGenomes.name);
-                    for (final File file : files) {
-                        final String json = txt.readTextFile(file);
-                        final Genome genome = Genome.fromJson(json);
-                        table.insert(genome.selected, genome.fitness, json);
-                    }
-                    return null;
-                }
-            };
-        } finally {
-            db.close();
-        }
-    }
-    
     public static SqlJetDb createDatabase(File dbFile) throws SqlJetException {
         Preconditions.checkNotNull(dbFile, "The parameter 'dbFile' must not be null");
-        Preconditions.checkArgument(!dbFile.exists(), "The parameter 'dbFile' must not exist");
+        Preconditions.checkArgument(!dbFile.exists(), "The parameter 'dbFile' must not exist (" + dbFile + ")");
         
         final SqlJetDb db = SqlJetDb.open(dbFile, true);
         new Transaction<Void>(db, SqlJetTransactionMode.WRITE) {
@@ -126,24 +94,6 @@ public class Database {
     protected abstract class Write<T> extends Transaction<T> {
         public Write() {
             super(database, SqlJetTransactionMode.WRITE);
-        }
-    }
-    
-    protected static class FileLister implements FileFilter {
-        
-        private final List<File> list;
-
-        public FileLister(List<File> list) {
-            Preconditions.checkNotNull(list, "The parameter 'list' must not be null");
-            this.list = list;
-        }
-        
-        @Override
-        public boolean accept(File pathname) {
-            if (pathname.isFile() && pathname.getName().endsWith(".genome")) {
-                list.add(pathname);
-            }
-            return false;
         }
     }
 }
---
+++ src/ch/brotzilla/monalisa/utils/OldFormatConverter.java
@@ -0,0 +1,70 @@
+package ch.brotzilla.monalisa.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.tmatesoft.sqljet.core.SqlJetException;
+import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
+import org.tmatesoft.sqljet.core.table.ISqlJetTable;
+import org.tmatesoft.sqljet.core.table.SqlJetDb;
+
+import ch.brotzilla.monalisa.db.Database;
+import ch.brotzilla.monalisa.db.DatabaseSchema;
+import ch.brotzilla.monalisa.db.Transaction;
+import ch.brotzilla.monalisa.evolution.genes.Genome;
+import ch.brotzilla.monalisa.io.TextReader;
+
+import com.google.common.base.Preconditions;
+
+public final class OldFormatConverter {
+
+    private OldFormatConverter() {}
+
+    public static void convertOldStorageFormat(File folder, File dbFile) throws SqlJetException, IOException {
+        Preconditions.checkNotNull(folder, "The parameter 'folder' must not be null");
+        Preconditions.checkArgument(folder.isDirectory(), "The parameter 'folder' has to be a directory");
+        
+        final LinkedList<File> files = new LinkedList<File>();
+        folder.listFiles(new FileLister(files));
+        
+        final SqlJetDb db = Database.createDatabase(dbFile);
+        try {
+            final TextReader txt = new TextReader(1024 * 100);
+            new Transaction<Void>(db, SqlJetTransactionMode.WRITE) {
+                @Override
+                public Void run() throws Exception {
+                    final ISqlJetTable table = db.getTable(DatabaseSchema.tblGenomes.name);
+                    for (final File file : files) {
+                        final String json = txt.readTextFile(file);
+                        final Genome genome = Genome.fromJson(json);
+                        table.insert(genome.selected, genome.fitness, json);
+                    }
+                    return null;
+                }
+            };
+        } finally {
+            db.close();
+        }
+    }
+
+    private static class FileLister implements FileFilter {
+        
+        private final List<File> list;
+
+        public FileLister(List<File> list) {
+            Preconditions.checkNotNull(list, "The parameter 'list' must not be null");
+            this.list = list;
+        }
+        
+        @Override
+        public boolean accept(File pathname) {
+            if (pathname.isFile() && pathname.getName().endsWith(".genome")) {
+                list.add(pathname);
+            }
+            return false;
+        }
+    }
+}
