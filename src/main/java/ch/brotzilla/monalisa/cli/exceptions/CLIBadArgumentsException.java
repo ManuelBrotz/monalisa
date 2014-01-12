84ac046663a5d94aa255a3d7e7aa54fb8f8e0f0a
Manuel Brotz <manu.brotz@gmx.ch>
2014-01-12 20:19:22 +0100

    Removed abstracts modifier from class CLIBadArgumentsException.

--- src/main/java/ch/brotzilla/monalisa/cli/exceptions/CLIBadArgumentsException.java
+++ src/main/java/ch/brotzilla/monalisa/cli/exceptions/CLIBadArgumentsException.java
@@ -3,7 +3,7 @@ package ch.brotzilla.monalisa.cli.exceptions;
 import com.beust.jcommander.JCommander;
 
 @SuppressWarnings("serial")
-public abstract class CLIBadArgumentsException extends CLIArgumentsException {
+public class CLIBadArgumentsException extends CLIArgumentsException {
 
     public CLIBadArgumentsException(String commandName, JCommander commander, Throwable cause) {
         super(commandName, commander, cause);
