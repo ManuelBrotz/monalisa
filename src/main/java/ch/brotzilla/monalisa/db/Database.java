48691423aab0c1e5c2e9a1ee230e283aca0bc945
Manuel Brotz <manu.brotz@gmx.ch>
2014-01-25 10:57:36 +0100

    Updated class Database to conform with the new database schema.

--- src/main/java/ch/brotzilla/monalisa/db/Database.java
+++ src/main/java/ch/brotzilla/monalisa/db/Database.java
@@ -1,15 +1,8 @@
 package ch.brotzilla.monalisa.db;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
-
-import ch.brotzilla.monalisa.evolution.genes.Genome;
-import ch.brotzilla.monalisa.images.ImageData;
-import ch.brotzilla.monalisa.utils.Compression;
 
 import com.almworks.sqlite4java.SQLiteBackup;
 import com.almworks.sqlite4java.SQLiteConnection;
@@ -20,42 +13,57 @@ import com.google.common.base.Preconditions;
 public class Database implements AutoCloseable {
 
     public static final DatabaseSchema Schema = new DatabaseSchema();
-    public static final String SelectLatestGenomeQuery = "SELECT selected, data FROM genomes ORDER BY selected DESC LIMIT 1";
-    public static final String SelectFileByIdQuery = "SELECT id, data FROM files WHERE id = ?1";
-    public static final String SelectAllFileIdsQuery = "SELECT id FROM files";
-    public static final String SelectAllFilesQuery = "SELECT id, originalName, compressed, data FROM files ORDER BY id ASC";
+
+    public static final String SelectLatestGenomeQuery = "SELECT improvements, fitness, data FROM genomes ORDER BY improvements DESC LIMIT 1";
+    public static final String SelectFileByIdQuery = "SELECT id, originalName, data FROM files WHERE id = ?1";
     public static final String SelectNumberOfGenomesQuery = "SELECT Count(selected) FROM genomes";
-    public static final String SelectAllGenomesQuery = "SELECT selected, data FROM genomes ORDER BY selected ASC";
+
+    public static final String SelectAllGenesQuery = "SELECT id, crc, data FROM genes ORDER BY id ASC";
+    public static final String SelectAllGenomesQuery = "SELECT improvements, fitness, data FROM genomes ORDER BY improvements ASC";
+    public static final String SelectAllFilesQuery = "SELECT id, originalName, data FROM files ORDER BY id ASC";
+
     public static final String InsertFileQuery = "INSERT INTO files (id, originalName, data) VALUES (?1, ?2, ?3)";
     public static final String InsertGenomeQuery = "INSERT INTO genomes (improvements, fitness, data) VALUES (?1, ?2, ?3)";
     public static final String InsertGeneQuery = "INSERT INTO genes (id, crc, data) VALUES (?1, ?2, ?3)";
 
     protected final SQLiteConnection conn;
 
-    protected final SQLiteStatement selectLatestGenomeQuery, selectNumberOfGenomesQuery, selectAllGenomesQuery, selectFileByIdQuery, selectAllFileIdsQuery, selectAllFilesQuery;
+    protected final SQLiteStatement selectLatestGenomeQuery, selectNumberOfGenomesQuery, selectFileByIdQuery;
+    protected final SQLiteStatement selectAllGenesQuery, selectAllGenomesQuery, selectAllFilesQuery;
     protected final SQLiteStatement insertFileQuery, insertGenomeQuery, insertGeneQuery;
 
     protected Transaction transaction;
-    
+
     private SQLiteStatement prepare(String sql) {
         try {
             return conn.prepare(sql);
         } catch (Exception e) {
-            System.out.println("Error: Unable to prepare sql statement: " + sql);
+            System.out.println("error: unable to prepare sql statement: " + sql);
+            System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    private void check(SQLiteStatement stmt) {
+        if (stmt == null) {
+            throw new IllegalStateException("Query not available");
+        }
     }
 
     private Database(SQLiteConnection conn) throws SQLiteException {
         Preconditions.checkNotNull(conn, "The parameter 'conn' must not be null");
         Preconditions.checkState(conn.isOpen(), "The connection to the database has to be open");
+
         this.conn = conn;
+
         this.selectLatestGenomeQuery = prepare(SelectLatestGenomeQuery);
         this.selectNumberOfGenomesQuery = prepare(SelectNumberOfGenomesQuery);
-        this.selectAllGenomesQuery = prepare(SelectAllGenomesQuery);
         this.selectFileByIdQuery = prepare(SelectFileByIdQuery);
-        this.selectAllFileIdsQuery = prepare(SelectAllFileIdsQuery);
+
+        this.selectAllGenesQuery = prepare(SelectAllGenesQuery);
+        this.selectAllGenomesQuery = prepare(SelectAllGenomesQuery);
         this.selectAllFilesQuery = prepare(SelectAllFilesQuery);
+
         this.insertFileQuery = prepare(InsertFileQuery);
         this.insertGenomeQuery = prepare(InsertGenomeQuery);
         this.insertGeneQuery = prepare(InsertGeneQuery);
@@ -66,6 +74,7 @@ public class Database implements AutoCloseable {
     }
 
     public int queryNumberOfGenomes() throws SQLiteException {
+        check(selectNumberOfGenomesQuery);
         selectNumberOfGenomesQuery.reset();
         if (selectNumberOfGenomesQuery.step()) {
             return selectNumberOfGenomesQuery.columnInt(0);
@@ -73,67 +82,67 @@ public class Database implements AutoCloseable {
         return -1;
     }
 
-    public int queryAllGenomesCompressed(List<byte[]> output) throws SQLiteException {
+    public GenomeEntry queryLatestGenome() throws SQLiteException, IOException {
+        check(selectLatestGenomeQuery);
+        selectLatestGenomeQuery.reset();
+        if (selectLatestGenomeQuery.step()) {
+            return new GenomeEntry(selectLatestGenomeQuery.columnInt(0), selectLatestGenomeQuery.columnDouble(1), selectLatestGenomeQuery.columnBlob(2));
+        }
+        return null;
+    }
+
+    public int queryAllGenes(List<GeneEntry> output) throws SQLiteException {
         Preconditions.checkNotNull(output, "The parameter 'output' must not be null");
+        check(selectAllGenesQuery);
+        selectAllGenesQuery.reset();
+        int count = 0;
+        while (selectAllGenesQuery.step()) {
+            output.add(new GeneEntry(selectAllGenesQuery.columnInt(0), selectAllGenesQuery.columnInt(1), selectAllGenesQuery.columnBlob(2)));
+            ++count;
+        }
+        return count;
+    }
+
+    public int queryAllGenomes(List<GenomeEntry> output) throws SQLiteException {
+        Preconditions.checkNotNull(output, "The parameter 'output' must not be null");
+        check(selectAllGenomesQuery);
         selectAllGenomesQuery.reset();
         int count = 0;
         while (selectAllGenomesQuery.step()) {
-            output.add(selectAllGenomesQuery.columnBlob(1));
+            output.add(new GenomeEntry(selectAllGenomesQuery.columnInt(0), selectAllGenomesQuery.columnDouble(1), selectAllGenomesQuery.columnBlob(2)));
             ++count;
         }
         return count;
     }
 
-    public Genome queryLatestGenome() throws SQLiteException, IOException {
-        selectLatestGenomeQuery.reset();
-        if (selectLatestGenomeQuery.step()) {
-            return Compression.decodeGenome(selectLatestGenomeQuery.columnBlob(1));
+    public int queryAllFiles(List<FileEntry> output) throws SQLiteException {
+        Preconditions.checkNotNull(output, "The parameter 'output' must not be null");
+        check(selectAllFilesQuery);
+        selectAllFilesQuery.reset();
+        int count = 0;
+        while (selectAllFilesQuery.step()) {
+            output.add(new FileEntry(selectAllFilesQuery.columnString(0), selectAllFilesQuery.columnString(1), selectAllFilesQuery.columnBlob(2)));
+            ++count;
         }
-        return null;
+        return count;
     }
 
-    public ImageData queryImage(String id) throws SQLiteException, IOException {
+    public FileEntry queryFileById(String id) throws SQLiteException {
+        Preconditions.checkNotNull(id, "The parameter 'id' must not be null");
+        check(selectFileByIdQuery);
         selectFileByIdQuery.reset();
         selectFileByIdQuery.bind(1, id);
         if (selectFileByIdQuery.step()) {
-            return Compression.decodeImageData(selectFileByIdQuery.columnBlob(1));
+            return new FileEntry(selectFileByIdQuery.columnString(0), selectFileByIdQuery.columnString(1), selectFileByIdQuery.columnBlob(2));
         }
         return null;
-    }
-    
-    public int queryAllFileIds(List<String> output) throws SQLiteException {
-        Preconditions.checkNotNull(output, "The parameter 'output' must not be null");
-        selectAllFileIdsQuery.reset();
-        int count = 0;
-        while (selectAllFileIdsQuery.step()) {
-            output.add(selectAllFileIdsQuery.columnString(0));
-            ++count;
-        }
-        return count;
-    }
-    
-    public void queryAllFiles(List<FileEntry> output) throws SQLiteException {
-        selectAllFilesQuery.reset();
-        while (selectAllFilesQuery.step()) {
-            output.add(new FileEntry(
-                    selectAllFilesQuery.columnString(0),
-                    selectAllFilesQuery.columnString(1),
-                    selectAllFilesQuery.columnInt(2) != 0,
-                    selectAllFilesQuery.columnBlob(3)
-                    ));
-        }
-    }
-
-    public void insertImage(String id, String originalName, ImageData data) throws IOException, SQLiteException {
-        throw new UnsupportedOperationException();
-//        final byte[] encoded = Compression.encode(data);
-//        insertFile(id, originalName, true, encoded);
     }
 
     public void insertFile(String id, String originalName, byte[] data) throws SQLiteException {
         Preconditions.checkNotNull(id, "The parameter 'id' must not be null");
         Preconditions.checkArgument(!id.isEmpty(), "The parameter 'id' must not be empty");
         Preconditions.checkNotNull(data, "The parameter 'data' must not be null");
+        check(insertFileQuery);
         insertFileQuery.reset();
         insertFileQuery.bind(1, id);
         insertFileQuery.bind(2, originalName);
@@ -141,14 +150,10 @@ public class Database implements AutoCloseable {
         insertFileQuery.step();
     }
 
-    public void insertGenome(Genome genome) throws IOException, SQLiteException {
-        throw new UnsupportedOperationException();
-//        Preconditions.checkNotNull(genome, "The parameter 'genome' must not be null");
-//        final byte[] encoded = Compression.encode(genome);
-//        insertGenome(genome.fitness, genome.numberOfImprovements, genome.genes.length, encoded);
-    }
-
     public void insertGenome(int improvements, double fitness, byte[] data) throws SQLiteException {
+        Preconditions.checkArgument(improvements >= 0, "The parameter 'improvements' has to be greater than or equal to zero");
+        Preconditions.checkNotNull(data, "The parameter 'data' must not be null");
+        check(insertGenomeQuery);
         insertGenomeQuery.reset();
         insertGenomeQuery.bind(1, improvements);
         insertGenomeQuery.bind(2, fitness);
@@ -157,6 +162,9 @@ public class Database implements AutoCloseable {
     }
 
     public void insertGene(int index, int crc, byte[] data) throws SQLiteException {
+        Preconditions.checkArgument(index > 0, "The parameter 'index' has to be greater than zero");
+        Preconditions.checkNotNull(data, "The parameter 'data' must not be null");
+        check(insertGeneQuery);
         insertGeneQuery.reset();
         insertGeneQuery.bind(1, index);
         insertGeneQuery.bind(2, crc);
@@ -263,51 +271,81 @@ public class Database implements AutoCloseable {
             leave();
         }
     }
-    
-    public static class FileEntry {
-        
+
+    public static class GeneEntry {
+
+        private final int id, crc;
         private final byte[] data;
-        private final String id, originalName;
-        private final boolean compressed;
-        
-        private byte[] decoded = null;
-        
-        public FileEntry(String id, String originalName, boolean compressed, byte[] data) {
+
+        public GeneEntry(int id, int crc, byte[] data) {
+            Preconditions.checkArgument(id > 0, "The parameter 'id' has to be greater than zero");
+            Preconditions.checkNotNull(data, "The parameter 'data' must not be null");
             this.id = id;
-            this.originalName = originalName;
-            this.compressed = compressed;
+            this.crc = crc;
             this.data = data;
         }
-        
+
+        public int getId() {
+            return id;
+        }
+
+        public int getCrc() {
+            return crc;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+    }
+
+    public static class GenomeEntry {
+
+        private final int improvements;
+        private final double fitness;
+        private final byte[] data;
+
+        public GenomeEntry(int improvements, double fitness, byte[] data) {
+            Preconditions.checkArgument(improvements >= 0, "The parameter 'improvements' has to be greater than or equal to zero");
+            Preconditions.checkNotNull(data, "The parameter 'data' must not be null");
+            this.improvements = improvements;
+            this.fitness = fitness;
+            this.data = data;
+        }
+
+        public int getImprovements() {
+            return improvements;
+        }
+
+        public double getFitness() {
+            return fitness;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+    }
+
+    public static class FileEntry {
+
+        private final String id, originalName;
+        private final byte[] data;
+
+        public FileEntry(String id, String originalName, byte[] data) {
+            this.id = id;
+            this.originalName = originalName;
+            this.data = data;
+        }
+
         public String getId() {
             return id;
         }
-        
+
         public String getOriginalName() {
             return originalName;
         }
-        
-        public byte[] decode() throws IOException {
-            if (!compressed) {
-                return data;
-            }
-            if (decoded != null) {
-                return decoded;
-            }
-            
-            final ByteArrayInputStream bin = new ByteArrayInputStream(data);
-            final GZIPInputStream gzin = new GZIPInputStream(bin);
-            final ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            final byte[] buf = new byte[1024];
-            
-            int read = gzin.read(buf, 0, buf.length);
-            while (read > 0) {
-                bout.write(buf, 0, read);
-                read = gzin.read(buf, 0, buf.length);
-            }
-            
-            decoded = bout.toByteArray();
-            return decoded;
+
+        public byte[] getData() throws IOException {
+            return data;
         }
     }
 }
