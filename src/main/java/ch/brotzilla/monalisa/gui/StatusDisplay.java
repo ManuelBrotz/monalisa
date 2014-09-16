b3dd7c2d3d195858d759dd1726cac300ef983643
Manuel Brotz <manu.brotz@gmx.ch>
2014-09-16 15:05:58 +0200

    Removed unused entries in StatusDisplay.

--- src/main/java/ch/brotzilla/monalisa/gui/StatusDisplay.java
+++ src/main/java/ch/brotzilla/monalisa/gui/StatusDisplay.java
@@ -19,11 +19,9 @@ public class StatusDisplay extends JPanel {
     
     protected final JLabel generatedName, generatedValue;
     protected final JLabel selectedName, selectedValue;
-    protected final JLabel mutationsName, mutationsValue;
     protected final JLabel polygonsName, polygonsValue;
     protected final JLabel pointsName, pointsValue;
     protected final JLabel fitnessName, fitnessValue;
-    protected final JLabel cacheName, cacheValue;
     protected final JLabel rateName, rateValue;
     
     protected final DecimalFormat ff = new DecimalFormat( "#,###,###,###,##0.00" );
@@ -56,16 +54,12 @@ public class StatusDisplay extends JPanel {
         generatedValue = new JLabel("0");
         selectedName = new JLabel("Selected:");
         selectedValue = new JLabel("0");
-        mutationsName = new JLabel("Mutations:");
-        mutationsValue = new JLabel("0");
         polygonsName = new JLabel("Polygons:");
         polygonsValue = new JLabel("0");
         pointsName = new JLabel("Points:");
         pointsValue = new JLabel("0");
         fitnessName = new JLabel("Fitness:");
         fitnessValue = new JLabel("0");
-        cacheName = new JLabel("Cached:");
-        cacheValue = new JLabel("0");
         rateName = new JLabel("Rate:");
         rateValue = new JLabel("0.00/s");
         
@@ -73,38 +67,30 @@ public class StatusDisplay extends JPanel {
         case Horizontal:
             add(generatedName, "row=1 col=2 hAlign=Right");
             add(selectedName, "row=Next col=Current hAlign=Right");
-            add(mutationsName, "row=Next col=Current hAlign=Right");
-            add(cacheName, "row=Next col=Current hAlign=Right");
+            add(rateName, "row=Next col=Current hAlign=Right");
             add(polygonsName, "row=1 col=5 hAlign=Right");
             add(pointsName, "row=Next col=Current hAlign=Right");
             add(fitnessName, "row=Next col=Current hAlign=Right");
-            add(rateName, "row=Next col=Current hAlign=Right");
             add(generatedValue, "row=1 col=3 hAlign=Right");
             add(selectedValue, "row=Next col=Current hAlign=Right");
-            add(mutationsValue, "row=Next col=Current hAlign=Right");
-            add(cacheValue, "row=Next col=Current hAlign=Right");
+            add(rateValue, "row=Next col=Current hAlign=Right");
             add(polygonsValue, "row=1 col=6 hAlign=Right");
             add(pointsValue, "row=Next col=Current hAlign=Right");
             add(fitnessValue, "row=Next col=Current hAlign=Right");
-            add(rateValue, "row=Next col=Current hAlign=Right");
             break;
         case Vertical:
             add(generatedName, "row=1 col=1 hAlign=Right");
             add(selectedName, "row=Next col=Current hAlign=Right");
-            add(mutationsName, "row=Next col=Current hAlign=Right");
-            add(cacheName, "row=Next col=Current hAlign=Right");
+            add(rateName, "row=Next col=Current hAlign=Right");
             add(polygonsName, "row=Next col=Current hAlign=Right");
             add(pointsName, "row=Next col=Current hAlign=Right");
             add(fitnessName, "row=Next col=Current hAlign=Right");
-            add(rateName, "row=Next col=Current hAlign=Right");
             add(generatedValue, "row=1 col=2 hAlign=Right");
             add(selectedValue, "row=Next col=Current hAlign=Right");
-            add(mutationsValue, "row=Next col=Current hAlign=Right");
-            add(cacheValue, "row=Next col=Current hAlign=Right");
+            add(rateValue, "row=Next col=Current hAlign=Right");
             add(polygonsValue, "row=Next col=Current hAlign=Right");
             add(pointsValue, "row=Next col=Current hAlign=Right");
             add(fitnessValue, "row=Next col=Current hAlign=Right");
-            add(rateValue, "row=Next col=Current hAlign=Right");
             break;
         default:
             throw new IllegalArgumentException("Orientation not supported: " + orientation);
@@ -115,7 +101,6 @@ public class StatusDisplay extends JPanel {
         if (genome == null) {
             generatedValue.setText("0");
             selectedValue.setText("0");
-            mutationsValue.setText("0");
             polygonsValue.setText("0");
             pointsValue.setText("0 (0.0)");
             fitnessValue.setText("0");
